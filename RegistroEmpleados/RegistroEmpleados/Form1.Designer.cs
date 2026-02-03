da177b99025d44d1d2642107896333d9e3febc5a
Roger <rogerha08@gmail.com>
2026-02-03 11:28:51 -0400

    Add files via upload

---
+++ RegistroEmpleados/RegistroEmpleados/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace RegistroEmpleados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.dtIngreso = new System.Windows.Forms.DateTimePicker();
+            this.cmbGenero = new System.Windows.Forms.ComboBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Dirección = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(453, 157);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(180, 31);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(201, 157);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(180, 31);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(201, 355);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(180, 31);
+            this.txtTelefono.TabIndex = 2;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(201, 223);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(180, 31);
+            this.txtApellido.TabIndex = 3;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(201, 296);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(180, 31);
+            this.txtDireccion.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(453, 296);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(180, 31);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.875F);
+            this.label1.Location = new System.Drawing.Point(222, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 37);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "“Registro de Empleados”";
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.Location = new System.Drawing.Point(453, 353);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(180, 31);
+            this.txtCargo.TabIndex = 7;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(453, 223);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(180, 31);
+            this.txtSalario.TabIndex = 8;
+            // 
+            // dtIngreso
+            // 
+            this.dtIngreso.Location = new System.Drawing.Point(642, 12);
+            this.dtIngreso.Name = "dtIngreso";
+            this.dtIngreso.Size = new System.Drawing.Size(200, 31);
+            this.dtIngreso.TabIndex = 9;
+            // 
+            // cmbGenero
+            // 
+            this.cmbGenero.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cmbGenero.FormattingEnabled = true;
+            this.cmbGenero.Location = new System.Drawing.Point(663, 432);
+            this.cmbGenero.Name = "cmbGenero";
+            this.cmbGenero.Size = new System.Drawing.Size(171, 33);
+            this.cmbGenero.TabIndex = 10;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(163, 434);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(139, 40);
+            this.btnSalir.TabIndex = 11;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(336, 434);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(133, 40);
+            this.btnAbrir.TabIndex = 12;
+            this.btnAbrir.Text = "Abrir archivo";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(502, 432);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(131, 41);
+            this.btnGuardar.TabIndex = 13;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(639, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 25);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 25);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Apellidos";
+            // 
+            // Dirección
+            // 
+            this.Dirección.AutoSize = true;
+            this.Dirección.Location = new System.Drawing.Point(79, 302);
+            this.Dirección.Name = "Dirección";
+            this.Dirección.Size = new System.Drawing.Size(102, 25);
+            this.Dirección.TabIndex = 17;
+            this.Dirección.Text = "Dirección";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 25);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Teléfono";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(639, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 25);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Salario";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(639, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 25);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Email";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(639, 358);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 25);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Cargo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(854, 527);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Dirección);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.cmbGenero);
+            this.Controls.Add(this.dtIngreso);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtCargo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.DateTimePicker dtIngreso;
+        private System.Windows.Forms.ComboBox cmbGenero;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Dirección;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ RegistroEmpleados/RegistroEmpleados/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace RegistroEmpleados
+{
+    public partial class Form1 : Form
+    {
+        string rutaArchivo = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnGuardar.Click += btnGuardar_Click;
+            btnAbrir.Click += btnAbrir_Click;
+            btnSalir.Click += btnSalir_Click;
+
+            txtId.TextChanged += Campos_TextChanged;
+            txtNombre.TextChanged += Campos_TextChanged;
+            txtApellido.TextChanged += Campos_TextChanged;
+            txtDireccion.TextChanged += Campos_TextChanged;
+            txtTelefono.TextChanged += Campos_TextChanged;
+            txtEmail.TextChanged += Campos_TextChanged;
+            txtCargo.TextChanged += Campos_TextChanged;
+            txtSalario.TextChanged += Campos_TextChanged;
+
+            cmbGenero.SelectedIndexChanged += cmbGenero_SelectedIndexChanged;
+
+            txtId.KeyPress += txtId_KeyPress;
+            txtTelefono.KeyPress += txtTelefono_KeyPress;
+            txtSalario.KeyPress += txtSalario_KeyPress;
+
+            cmbGenero.Items.Add("Masculino");
+            cmbGenero.Items.Add("Femenino");
+            cmbGenero.SelectedIndex = -1;
+
+            dtIngreso.MinDate = new DateTime(2000, 1, 1);
+            dtIngreso.MaxDate = DateTime.Now;
+
+            btnGuardar.Enabled = false;
+        }
+
+        private bool EsEmailValido(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pattern);
+        }
+
+        private bool ValidarCampos()
+        {
+            if (!int.TryParse(txtId.Text, out int id) || id <= 0)
+            {
+                MessageBox.Show("El ID debe ser un número positivo");
+                txtId.Focus();
+                return false;
+            }
+
+            if (txtNombre.Text.Length < 2)
+            {
+                MessageBox.Show("Nombre inválido");
+                txtNombre.Focus();
+                return false;
+            }
+
+            if (txtApellido.Text.Length < 2)
+            {
+                MessageBox.Show("Apellido inválido");
+                txtApellido.Focus();
+                return false;
+            }
+
+            if (!long.TryParse(txtTelefono.Text, out _))
+            {
+                MessageBox.Show("Teléfono inválido");
+                txtTelefono.Focus();
+                return false;
+            }
+
+            if (!EsEmailValido(txtEmail.Text))
+            {
+                MessageBox.Show("Email inválido");
+                txtEmail.Focus();
+                return false;
+            }
+
+            if (!decimal.TryParse(txtSalario.Text, out decimal salario) || salario <= 0)
+            {
+                MessageBox.Show("Salario inválido");
+                txtSalario.Focus();
+                return false;
+            }
+
+            if (cmbGenero.SelectedIndex == -1)
+            {
+                MessageBox.Show("Seleccione un género");
+                cmbGenero.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (!ValidarCampos()) return;
+
+            if (MessageBox.Show("¿Desea guardar los datos?", "Confirmar", MessageBoxButtons.YesNo) != DialogResult.Yes)
+                return;
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Archivo de texto (*.txt)|*.txt";
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                rutaArchivo = save.FileName;
+
+                string contenido =
+                    $"ID: {txtId.Text}\n" +
+                    $"Nombre: {txtNombre.Text}\n" +
+                    $"Apellido: {txtApellido.Text}\n" +
+                    $"Dirección: {txtDireccion.Text}\n" +
+                    $"Teléfono: {txtTelefono.Text}\n" +
+                    $"Email: {txtEmail.Text}\n" +
+                    $"Cargo: {txtCargo.Text}\n" +
+                    $"Salario: {txtSalario.Text}\n" +
+                    $"Género: {cmbGenero.Text}\n" +
+                    $"Fecha ingreso: {dtIngreso.Value:dd/MM/yyyy}\n";
+
+                File.WriteAllText(rutaArchivo, contenido);
+
+                MessageBox.Show("Archivo guardado correctamente");
+
+                if (MessageBox.Show("¿Desea abrir el archivo?", "Abrir", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Process.Start("notepad.exe", rutaArchivo);
+                }
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Desea borrar los datos?", "Confirmar", MessageBoxButtons.YesNo) != DialogResult.Yes)
+                return;
+
+            txtId.Clear();
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtDireccion.Clear();
+            txtTelefono.Clear();
+            txtEmail.Clear();
+            txtCargo.Clear();
+            txtSalario.Clear();
+
+            cmbGenero.SelectedIndex = -1;
+            dtIngreso.Value = DateTime.Now;
+            btnGuardar.Enabled = false;
+            txtId.Focus();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Archivos de texto (*.txt)|*.txt";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                Process.Start("notepad.exe", open.FileName);
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Desea salir?", "Salir", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Campos_TextChanged(object sender, EventArgs e)
+        {
+            HabilitarBotonGuardar();
+        }
+
+        private void cmbGenero_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HabilitarBotonGuardar();
+        }
+
+        private void HabilitarBotonGuardar()
+        {
+            btnGuardar.Enabled =
+                !string.IsNullOrWhiteSpace(txtId.Text) &&
+                !string.IsNullOrWhiteSpace(txtNombre.Text) &&
+                !string.IsNullOrWhiteSpace(txtApellido.Text) &&
+                cmbGenero.SelectedIndex != -1;
+        }
+
+        private void txtId_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void txtTelefono_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void txtSalario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+                e.Handled = true;
+
+            if (e.KeyChar == '.' && (sender as TextBox).Text.Contains("."))
+                e.Handled = true;
+        }
+    }
+}
